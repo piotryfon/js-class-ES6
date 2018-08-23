cdf02d8b0505dae45bbbae683c0d07a6b6fcc633
piotryfon <pit.pit@wp.pl>
2018-08-23 11:54:45 +0200

    addToTable

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
-$(document).ready(function(){
-    class Osoba{
-        constructor(firstName,lastName,age){
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.age = age;
-            this.hello = function(){
-                $(".lista").append($(`<li>Cześć ${this.firstName} ${lastName},
-                 masz już ${this.age} lat.</li>`));
-            }
+class Osoba{
+    constructor(firstName,lastName,gender,age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+       
+        this.addToList = function(){
+            $(".lista").append($(`<li>Cześć ${this.firstName} ${lastName},
+             masz już ${this.age} lat.</li>`));
         }
     }
-    let osoba1 = new Osoba("Piotr", "Tryfon", 43);
-    osoba1.hello();
-    
-    let osoba2 = new Osoba("Ala","Mądrala",28);
-    osoba2.hello();
+}
+Osoba.prototype.addToTable = function(){
+    $("tbody").append($(`<tr><td>${this.firstName}</td><td>${this.lastName}</td>
+    <td>${this.gender}</td><td>${this.age}</td></tr>`))
+}
 
-    let osoba3 = new Osoba("Adam", "Słodowy", 23);
-    osoba3.hello();   
+$(document).ready(function(){
+    let osoba1 = new Osoba("Piotr", "Programista", "mężczyzna", 43);
+    osoba1.addToList();
+    osoba1.addToTable();
+    
+    let osoba2 = new Osoba("Ala","Mądrala", "kobieta", 28);
+    osoba2.addToList();
+    osoba2.addToTable();
+
+    let osoba3 = new Osoba("Adam", "Słodowy", "mężczyzna", 23);
+    osoba3.addToList(); 
+    osoba3.addToTable();  
+
 })
